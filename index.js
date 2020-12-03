22850390fdbc419033f61abc0b90d442db035bf5
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-12-03 18:30:45 +1100

    apply event listener to all images

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const activateCard = (id) => {
   card[id].classList.add("active");
 };
 
-card[0].addEventListener("click", () => {
-  activateCard(0);
-});
+for (let i = 0; i < card.length; i++) {
+  card[i].addEventListener("click", () => {
+    activateCard(i);
+  });
+}
