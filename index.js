7fc94fa18b10c3cd5f384b59e6314a8ca4c2b07b
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-12-03 18:34:27 +1100

    create toggle for activating cards

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var card = document.getElementsByClassName("card");
+let card = document.getElementsByClassName("card");
+let isActive = 0;
 
 const activateCard = (id) => {
+  card[isActive].classList.remove("active");
   card[id].classList.add("active");
+  isActive = id;
 };
 
 for (let i = 0; i < card.length; i++) {
