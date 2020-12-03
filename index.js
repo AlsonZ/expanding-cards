6c0c1e0655aee76287218c3f51484044a0d710f9
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-12-03 20:20:23 +1100

    add overlay and default image storage in localStorage

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 let card = document.getElementsByClassName("card");
 let isActive = 0;
 
+const setDefaultImages = () => {
+  let defaultImages = [
+    "https://picsum.photos/id/1022/800/600",
+    "https://picsum.photos/id/1035/800/600",
+    "https://picsum.photos/id/1049/800/600",
+    "https://picsum.photos/id/1051/800/600",
+    "https://picsum.photos/id/1004/800/600",
+  ];
+  if (window.localStorage.getItem("Image" + 0) == null) {
+    for (let id = 0; id < defaultImages.length; id++) {
+      window.localStorage.setItem("Image" + id, defaultImages[id]);
+    }
+  }
+};
+
 const activateCard = (id) => {
   card[isActive].classList.remove("active");
   card[id].classList.add("active");
@@ -15,8 +30,11 @@ const onCardClick = (id) => {
   }
 };
 
+setDefaultImages();
+
 for (let id = 0; id < card.length; id++) {
   card[id].addEventListener("click", () => {
     onCardClick(id);
   });
+  card[id].children[0].src = window.localStorage.getItem("Image" + id);
 }
